b8b23d565e28aa52ebf4a0c16863f44d536ba60b
Felix-Blom <felix.blom@live.nl>
2023-06-13 12:18:03 +0200

    y

---
+++ section-05-assignment/classification model/processing/features.py
@@ -0,0 +1,21 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class ExtractLetterTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self, variables):
+
+        if not isinstance(variables, list):
+            raise ValueError("Variables should be a list")
+
+        self.variables = variables
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+
+        for feature in self.variables:
+            X[feature] = X[feature].str[0]
+
+        return X
--- section-05-assignment/setup.py
+++ section-05-assignment/setup.py
@@ -5,12 +5,12 @@ from pathlib import Path
 from setuptools import find_packages, setup
 
 # Package meta-data.
-NAME = "tid-regression-model"
+NAME = "tid-titantic-classificatiion-model"
 DESCRIPTION = "Example regression model package from Train In Data."
 URL = "https://github.com/trainindata/testing-and-monitoring-ml-deployments"
 EMAIL = "christopher.samiullah@protonmail.com"
 AUTHOR = "ChristopherGS"
-REQUIRES_PYTHON = ">=3.6.0"
+REQUIRES_PYTHON = ">=3.7.0"
 
 
 # The rest you shouldn't have to touch too much :)
